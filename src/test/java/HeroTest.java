88edb10e15c2f1cb7f2b8b841ea5185fe5eecdd5
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-12 12:59:36 +0300

    tested the hero class

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,2 +1,48 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class HeroTest {
+    @Test
+    public void Hero_instantiatesCorrectly_true() {
+        Hero myHero = new Hero("Mike" ,16 ,"fire" ,"cats");
+        assertEquals(true, myHero instanceof Hero);
+    }
+    @Test
+    public void Hero_instantiatesWithName_String() {
+        Hero myHero = new Hero("Mike", 16, "fire", "cats");
+        assertEquals("Mike", myHero.getNewName());
+    }
+    @Test
+    public void Hero_instantiatesWithAge_Integer() {
+        Hero myHero = new Hero("Mike" ,16 ,"fire" ,"cats");
+        assertEquals(16, myHero.getNewAge());
+    }
+//    @Test
+//    public void Hero_instantiatesWithId_Integer() {
+//        Hero myHero = new Hero("Mike" ,16 ,"fire" ,"cats");
+//        assertEquals(1, myHero.getmId());
+//    }
+    @Test
+    public void Hero_instantiatesWithSpecialPowers_String() {
+        Hero myHero = new Hero("Mike" ,16 ,"fire" ,"cats");
+        assertEquals("fire", myHero.getNewSpecialpowers());
+    }
+    @Test
+    public void Hero_instantiatesWithWeakness_String() {
+        Hero myHero = new Hero("Mike" ,16 ,"fire" ,"cats");
+        assertEquals("cats", myHero.getNewWeakness());
+    }
+    @Test
+    public void all_returnsAllInstancesOfHero_true() {
+        Hero firstHero = new Hero("Mike" ,16 ,"fire" ,"cats");
+        Hero secondHero = new Hero("Mike" ,16 ,"fire" ,"cats");
+        assertEquals(true, Hero.all().contains(firstHero));
+        assertEquals(true, Hero.all().contains(secondHero));
+    }
+    @Test
+    public void find_returnsHeroWithSameId_secondHero() {
+        Hero firstHero = new Hero("Mike" ,16 ,"fire" ,"cats");
+        Hero secondHero = new Hero("Mike" ,16 ,"fire" ,"cats");
+        assertEquals(Hero.find(secondHero.getId()), secondHero);
+    }
 }
