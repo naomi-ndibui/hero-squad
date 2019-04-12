4a9668bbf449c95687146bcf893cd562f404644d
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-12 12:58:15 +0300

    imported and added get method

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,3 +1,53 @@
-public class Hero {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Hero {
+    private String newName;
+    private int newAge;
+    private int mId;
+    private String newSpecialPowers;
+    private String newWeakness;
+    private  static List<Hero> instances = new ArrayList<Hero>();
+
+    public Hero(String name,int age,String specialPowers,String weakness){
+        newName=name;
+        newAge=age;
+        newSpecialPowers=specialPowers;
+        newWeakness=weakness;
+        instances.add(this);
+        mId= instances.size();
+    }
+
+    public String getNewName() {
+        return newName;
+    }
+
+    public int getNewAge() {
+        return newAge;
+    }
+
+    public String getNewSpecialpowers() {
+
+        return newSpecialPowers;
+    }
+
+    public String getNewWeakness() {
+
+        return newWeakness;
+    }
+
+    public static  List<Hero> all(){
+
+        return instances;
+    }
+
+    public int getId() {
+
+        return mId;
+    }
+
+    public static Hero find(int id) {
+
+        return instances.get(id - 1);
+    }
 }
