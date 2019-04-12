98936181ea69c247f48e3139f7606e4dd953abb9
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-12 15:47:35 +0300

    added imports and get methods

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,2 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squad {
+    private String mName;
+    private int mMaxSize;
+    private String mCause;
+    private static List<Squad> instances = new ArrayList<>();
+    private int mId;
+    private List<Hero> mHeros;
+
+    public Squad(String name, int maxSize, String cause) {
+        mName = name;
+        mMaxSize = maxSize;
+        mCause = cause;
+        instances.add(this);
+        mId = instances.size();
+        mHeros = new ArrayList<Hero>();
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public int getmMaxSize() {
+        return mMaxSize;
+    }
+
+    public String getmCause() {
+        return mCause;
+    }
+
+    public static List<Squad> all() {
+        return instances;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public static Squad find(int id) {
+        try {
+            return instances.get(id - 1);
+        } catch (IndexOutOfBoundsException exception) {
+            return null;
+        }
+    }
+
+    public static void clear()
+    {
+        instances.clear();
+    }
+    public List<Hero> getHeroes() {
+        return mHeros;
+    }
+    public void addHero(Hero hero) {
+        mHeros.add(hero);
+    }
 }
