62210280eb12511361e137b0ccc2d055cd6cbaa6
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-12 12:50:26 +0300

    imported and added get method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,27 @@
+import spark.ModelAndView;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+
+//import java.util.List;
+//import java.util.ArrayList;
+
+
+
 public class App {
      public static void main(String[] args) {
+         staticFileLocation("/public");
          String layout = "templates/layout.vtl";
+
+
+         get("/", (request, response) -> {
+             Map<String, Object> model = new HashMap<String, Object>();
+             model.put("template", "templates/hero-form.vtl");
+             return new ModelAndView(model, layout);
+         },  new VelocityTemplateEngine());
+
 
      }
 }
